a971d34355bfc93057c17817b59d7d295a9c666e
PawelPavo <Pawelebllc@gmail.com>
2020-02-03 10:36:02 -0500

    final refactored

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const maxArea = 600;
-function randomVal(min, max) {
-    return Math.floor(Math.random() * (max - min));
-}
-
 $('#start-btn').click(() => {
     $('.side-bar').css('width', '350px')
     $('#display-area').css('height', '600px')
@@ -13,167 +8,121 @@ $('#close-btn').click(() => {
 
 });
 
+
 class Shape {
     constructor(height, width) {
-        this.height = height
-        this.width = width        
-        this.updateLocation()
+        this.height = height;
+        this.width = width;
+        this.render()
+        this.div.dblclick(() => this.remove());
+        this.div.click(() => this.describe());
     }
+
+    render() {
+        this.div = $('<div class="shape"></div>')
+        this.div.css({
+            height: `${this.height}`,
+            width: `${this.width}`,
+            top: this.randomLocation(this.height),
+            left: this.randomLocation(this.width)
+        })
+        $('#display-area').append(this.div)
+    }
+
+    randomLocation(offset) {
+        return Math.floor(Math.random() * (600 - offset))
+    }
+
+    describe() {
+        $('#name-output').val(this.div.attr('id'));
+        $('#radius-output').val('N/A')
+        $('#height-output').val(this.height)
+        $('#width-output').val(this.width)
+        $('#area-output').val(this.area = this.height * this.width)
+        $('#perm-output').val(this.perm = 2 * this.height + 2 * this.width)
+    }
+
+    remove() {
+        $(this.div).remove()
+        $('#name-output').val('')
+        $('#area-output').val('')
+        $('#height-output').val('')
+        $('#width-output').val('')
+        $('#radius-output').val('')
+        $('#perm-output').val('')
+        $('#triangle-input').val('')
+    }
+
 }
 
 class Square extends Shape {
     constructor(height) {
         super(height, height)
-        this.div = $('#display-area').append('<div id="square"></div>')
-        this.width = $('#square').css({
-            'width': `${height}px`,
-            'height': `${height}px`
-        })
-        this.updateLocation();
-        this.div = $('#square').click(() => {
-            $('#name-output').val('Square')
-            $('#area-output').val(height * height)
-            $('#height-output').val(height)
-            $('#width-output').val(height)
-            $('#radius-output').val('N/A')
-            $('#perm-output').val(height * 4)
-        })
-        this.div = $('#square').dblclick(() =>{
-            $(this.div).remove()
-            $('#name-output').val('')
-            $('#area-output').val('')
-            $('#height-output').val('')
-            $('#width-output').val('')
-            $('#radius-output').val('')
-            $('#perm-output').val('')
-            $('#square-input').val('')
-        })
+        this.div.attr('id', 'square')
     }
-    updateLocation() {
-        let locationX = randomVal(this.width, maxArea)
-        let locationY = randomVal(this.height, maxArea)
-        this.div = $('#square').css({
-            'left': `${locationX}px`,
-            'top': `${locationY}px`,
-        })
+    remove() {
+        super.remove();
+        $('#square-input').val('')
     }
 }
+
 class Rectangle extends Shape {
     constructor(height, width) {
-        super(height, width);
-        this.div = $('#display-area').append('<div id="rectangle"></div>')
-        this.width = $('#rectangle').css({
-            'height': `${height}px`,
-            'width': `${width}px`
-        })
-        this.updateLocation()
-        this.div = $('#rectangle').click(() => {
-            $('#name-output').val('Rectangle')
-            $('#area-output').val(height * width)
-            $('#height-output').val(height)
-            $('#width-output').val(width)
-            $('#radius-output').val('N/A')
-            $('#perm-output').val((height * 2) + (width * 2))
-        })
-        this.div = $('#rectangle').dblclick(() =>{
-            $(this.div).remove()
-            $('#name-output').val('')
-            $('#area-output').val('')
-            $('#height-output').val('')
-            $('#width-output').val('')
-            $('#radius-output').val('')
-            $('#perm-output').val('')
-            $('#rectangle-input-1').val('')
-            $('#rectangle-input-2').val('')
-        })
+        super(height, width)
+        this.div.attr('id', 'rectangle')
     }
-    updateLocation() {
-        let locationX = randomVal(this.width, maxArea)
-        let locationY = randomVal(this.height, maxArea)
-        this.div = $('#rectangle').css({
-            'left': `${locationX}px`,
-            'top': `${locationY}px`,
-        })
+    remove() {
+        super.remove();
+        $('#rectangle-input-1').val('')
+        $('#rectangle-input-2').val('')
     }
 }
+
 class Circle extends Shape {
     constructor(radius) {
         super(radius * 2, radius * 2);
+        this.div.attr('id', 'circle')
         this.radius = radius
-        this.div = $('#display-area').append('<div id="circle"></div>')
-        this.width = $('#circle').css({
-            'width': `${this.radius * 2}px`,
-            'height': `${this.radius * 2}px`
-        })
-        this.updateLocation();
-        this.div = $('#circle').click(() => {
-            $('#name-output').val('Circle')
-            $('#area-output').val(3.14 * (this.radius * this.radius))
-            $('#height-output').val('N/A')
-            $('#width-output').val('N/A')
-            $('#radius-output').val(this.radius)
-            $('#perm-output').val(2 * 3.14 * this.radius)
-        })
-        this.div = $('#circle').dblclick(() =>{
-            $(this.div).remove()
-            $('#name-output').val('')
-            $('#area-output').val('')
-            $('#height-output').val('')
-            $('#width-output').val('')
-            $('#radius-output').val('')
-            $('#perm-output').val('')
-            $('#circle-input').val('')
-        })
     }
-    updateLocation() {
-        let locationX = randomVal(this.radius*2, maxArea)
-        let locationY = randomVal(this.radius*2, maxArea)
-        this.div = $('#circle').css({
-            'left': `${locationX}px`,
-            'top': `${locationY}px`,
-        })
+
+    describe() {
+        super.describe();
+        $('#area-output').val(Math.PI * Math.pow(this.radius, 2))
+        $('#radius-output').val(this.radius)
+        $('#perm-output').val(2 * Math.PI * this.radius)
+    }
+
+    remove() {
+        super.remove();
+        $('#circle-input').val('')
     }
 }
+
 class Triangle extends Shape {
     constructor(height) {
         super(height, height)
-        this.div = $('#display-area').append('<div id="triangle"></div>')
-        this.right = $('#triangle').css({
-            'border-right': `${height}px solid transparent`,
-        })
-        this.bottom = $('#triangle').css({
-            'border-bottom': `${height}px solid yellow`
-        })
-        this.updateLocation();
-        this.div = $('#triangle').click(() => {
-            $('#name-output').val('Triangle')
-            $('#area-output').val((height * height) / 2)
-            $('#height-output').val(height)
-            $('#width-output').val(height)
-            $('#radius-output').val('N/A')
-            let perm = ((Math.sqrt(2 * (Math.pow(height, 2)))) + (height * 2))
-            $('#perm-output').val(perm)
-        })
-        this.div = $('#triangle').dblclick(() =>{
-            $(this.div).remove()
-            $('#name-output').val('')
-            $('#area-output').val('')
-            $('#height-output').val('')
-            $('#width-output').val('')
-            $('#radius-output').val('')
-            $('#perm-output').val('')
-            $('#tiangle-input').val('')
+        this.div.attr('id', 'triangle')
+        $('#triangle').css({
+            'border-right': `${this.height}px solid transparent`,
+            'border-bottom': `${this.width}px solid yellow`
         })
     }
-    updateLocation() {
-        let locationX = randomVal(this.width, maxArea)
-        let locationY = randomVal(this.height, maxArea)
-        this.div = $('#triangle').css({
-            'left': `${locationX}px`,
-            'top': `${locationY}px`,
+
+    render() {
+        super.render()
+        this.div.css({
+            height: `0px`,
+            width: `0px`,
         })
+    }
+
+    describe() {
+        super.describe();
+        $('#area-output').val((this.height * this.width) / 2)
+        $('#perm-output').val(2 * this.height + Math.sqrt(2) * this.height)
     }
 }
+
 
 $('#square-btn').click(() => {
     let squareHeight = $('#square-input').val()
